65156b9ba2e3636e22634c0908d8e1c789d6a233
cher-di <cher-di@mail.ru>
2021-10-17 22:59:34 +0500

    Initial tower script

---
+++ tower.py
@@ -0,0 +1,84 @@
+import argparse
+import logging
+import signal
+import sys
+import time
+
+import gpiozero
+import psutil
+
+from collections import deque
+from typing import Sequence, Callable
+
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(message)s')
+
+
+AVAILABLE_GPIO_PINS = list(range(2, 28))
+
+
+def current_cpu_temperature():
+    return psutil.sensors_temperatures()['cpu_thermal'][0].current
+
+
+def count_percentage(values: Sequence, comparator: Callable):
+    suitable = list(filter(comparator, values))
+    return len(suitable) / len(values) * 100
+
+
+def parse_args(args: list) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='Switch on/off ice tower',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('pin',
+                        help='Num of gpio pin',
+                        type=int,
+                        choices=AVAILABLE_GPIO_PINS)
+    parser.add_argument('-d', '--delay',
+                        dest='delay',
+                        help='Delay between measurements (seconds)',
+                        type=int,
+                        default=1)
+    parser.add_argument('-t', '--temperature',
+                        dest='temperature',
+                        help='Temperature limit (celsius degress)',
+                        type=float,
+                        default=40)
+    parser.add_argument('-w', '--window',
+                        dest='window',
+                        help='Measurements window',
+                        type=int,
+                        default=10)
+    parser.add_argument('-p', '--percentage',
+                        dest='percentage',
+                        help='Percentage of change',
+                        type=float,
+                        default=80.0)
+    return parser.parse_args(args)
+
+
+def main(args: dict):
+    tower = gpiozero.OutputDevice(args['pin'])
+    measurements = deque(maxlen=args['window'])
+    while True:
+        time.sleep(args['delay'])
+        measurements.append(current_cpu_temperature())
+        if tower.value == 0:
+            actual_percentage = count_percentage(
+                measurements, lambda x: x > args['temperature'])
+            if actual_percentage > args['percentage']:
+                logging.info('ON ise tower')
+                tower.on()
+        else:
+            actual_percentage = count_percentage(
+                measurements, lambda x: x < args['temperature'])
+            if actual_percentage > args['percentage']:
+                logging.info('OFF ice tower')
+                tower.off()
+
+
+if __name__ == '__main__':
+    args = parse_args(sys.argv[1:])
+    main(vars(args))
